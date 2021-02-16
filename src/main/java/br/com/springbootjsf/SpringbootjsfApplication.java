b3e7a331b918b5dfa6a65f40987978d50e9a65f7
Ailton <tym.ailton@gmail.com>
2021-02-16 16:19:20 -0300

    iniciando projeto

---
+++ src/main/java/br/com/springbootjsf/SpringbootjsfApplication.java
@@ -0,0 +1,13 @@
+package br.com.springbootjsf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootjsfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootjsfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/springbootjsf/SpringbootjsfApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.springbootjsf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootjsfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
